6ce5c5c8ff64b5bff81b749d03c7a98078188d7f
Camiel Adriaens <145980502+CamielAdriaens@users.noreply.github.com>
2024-10-10 16:55:15 +0200

    merge conflict markers deleted

--- FileStorage/Database/MongoDbSettings.cs
+++ FileStorage/Database/MongoDbSettings.cs
@@ -2,10 +2,7 @@
 {
     public class MongoDbSettings
     {
-<<<<<<< Updated upstream
         public string ConnectionString { get; set; }
         public string DatabaseName { get; set; }
-=======
->>>>>>> Stashed changes
     }
 }
