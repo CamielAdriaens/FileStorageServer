954eb0cabe76f6cbb0ad55d9f1383d9e604c9197
CamielAdriaens <camillothee@gmail.com>
2024-11-06 19:21:25 +0100

    a

--- DTOs/obj/Debug/net6.0/DTOs.AssemblyInfo.cs
+++ DTOs/obj/Debug/net6.0/DTOs.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DTOs")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6c4cbbe7bbf00798344669202bf343c8e6cca5ce")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c2ec0348b2acb13b3b43e5b38c20a33fdf89d864")]
 [assembly: System.Reflection.AssemblyProductAttribute("DTOs")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DTOs")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
